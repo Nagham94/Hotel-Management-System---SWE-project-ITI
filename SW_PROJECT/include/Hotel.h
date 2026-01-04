3e4ec2da640712c1ffe39b2b152c25828a560c3b
somaya <somaya189ayman@gmail.com>
2026-01-04 03:04:05 +0200

    add hotel class

--- SW_PROJECT/include/Hotel.h
+++ SW_PROJECT/include/Hotel.h
@@ -1,16 +1,26 @@
 #ifndef HOTEL_H
 #define HOTEL_H
 
+#include <vector>
+#include "Room.h"
+#include "Booking.h"
 
 class Hotel
 {
     public:
         Hotel();
         virtual ~Hotel();
-
-    protected:
+        void addRoom(Room room);
+        void addBooking(Booking booking);
+        Room* findAvailableRoom();
+        void showAllRooms();
+        void showAllBookings();
 
     private:
+        vector<Room> rooms;
+        vector<Booking> bookings;
 };
 
 #endif // HOTEL_H
+
+
--- SW_PROJECT/src/Hotel.cpp
+++ SW_PROJECT/src/Hotel.cpp
@@ -1,4 +1,7 @@
 #include "Hotel.h"
+#include <iostream>
+
+using namespace std;
 
 Hotel::Hotel()
 {
@@ -8,4 +11,49 @@ Hotel::Hotel()
 Hotel::~Hotel()
 {
     //dtor
+}
+
+void Hotel::addRoom(Room room)
+{
+    rooms.push_back(room);
+}
+
+void Hotel::addBooking(Booking booking)
+{
+    bookings.push_back(booking);
+}
+
+Room* Hotel::findAvailableRoom()
+{
+    for (auto& room : rooms)
+    {
+        if(room.checkAvailability())
+        {
+            return &room;
+        }
+    }
+    return nullptr;
+}
+
+void Hotel::showAllRooms()
+{
+    for (const auto& room : rooms)
+    {
+        cout << "Room Number: " << room.getRoomNumber() << endl;
+        cout << "Type: " << room.getType() << endl;
+        cout << "Price per Night: " << room.getPrice() << endl;
+        cout << "Available: "
+             << (room.checkAvailability() ? "Yes" : "No") << endl;
+        cout << "----------------------" << endl;
+    }
+}
+void Hotel::showAllBookings()
+{
+    for (const auto& booking : bookings)
+    {
+        cout << "Booking ID: " << booking.getBookingId() << endl;
+        cout << "Guest Name: " << booking.getGuest().getName() << endl;
+        cout << "Total Price: " << booking.calculateTotal() << endl;
+        cout << "----------------------" << endl;
+    }
 }
